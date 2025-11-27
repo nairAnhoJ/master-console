0e90df5704397db8568b600de8e7f2d1ab65bfaa
nairAnhoJ <malondrasarian@gmail.com>
2025-11-27 16:26:35 +0800

    Area Slice Added

---
+++ src/features/areas/areaSlice.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import config from "../../config/config";
+
+export interface AreaList {
+    id: number;
+    name: string;
+}
+
+interface InitialState {
+    areas: AreaList[];
+    loading: boolean;
+}
+
+const initialState: InitialState = {
+    areas: [],
+    loading: false
+}
+
+export const fetchArea = createAsyncThunk('areas/fetch', async () => {
+    try {
+        const res = await config.get('/areas');
+        return res.data;
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+const areaSlice = createSlice({
+    name: 'area',
+    initialState,
+    reducers: {},
+    extraReducers: (builder => {
+        builder
+            .addCase(fetchArea.fulfilled, (state, action) => {
+                state.areas = action.payload;
+            })
+    })
+})
+
+export default areaSlice.reducer;
