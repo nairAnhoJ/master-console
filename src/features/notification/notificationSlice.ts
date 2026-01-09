a7762d998e130b9c321530bd1d4094e69feaf1bd
nairAnhoJ <malondrasarian@gmail.com>
2026-01-09 10:37:00 +0800

    clean notificationSlice

--- src/features/notification/notificationSlice.ts
+++ src/features/notification/notificationSlice.ts
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import reducer from "../auth/authSlice";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface Notification {
     feature: 'users' | 'departments' | 'areas' | 'sites' | null;
